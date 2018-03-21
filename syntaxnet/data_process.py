1f0f6f463b8dd6f5d73dd6266bed43e99223b1a2
toni <tkukurin@gmail.com>
2018-03-21 13:17:39 +0100

    some data process

--- syntaxnet/data_process.py
+++ syntaxnet/data_process.py
@@ -9,25 +9,22 @@ TWEET_FILE = os.path.join(DATA, 'tweets.json')
 
 def compose(*fs):
   return lambda value: \
-    functools.reduce(lambda f, v: f(v), fs, value)
+    functools.reduce(lambda v, f: f(v), fs, value)
 
 
 def remove_crap(tweet_content):
   atreplies = r'(@[A-Za-z0-9_]+)'
   urls = r'http:\S+'
-  not_word = r'([^0-9A-Za-z \t])'
 
-  regex = '|'.join(v for k, v in locals() if k != 'tweet_content')
+  regex = '|'.join(
+      v for k, v in locals().items()
+      if k != 'tweet_content')
   return re.sub(regex, '', tweet_content)
 
 
-def to_utf8(unicode_):
-  return unicode_.encode('utf-8')
-
-
 def normalize(tweets):
+  print(tweets)
   pipeline = compose(
-      to_utf8,
       str.lower,
       remove_crap,
       str.strip)
@@ -37,5 +34,6 @@ def normalize(tweets):
 
 if __name__ == '__main__':
   tweets = pd.read_json(TWEET_FILE)
-  content = tweets['body']['content']
+  content = tweets['body'].apply(lambda dict_: dict_['content'])
+  print(list(normalize(content[:1])))
 
