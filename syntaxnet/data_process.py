05fcd4155cd87cd43e8ddf68228c2308dd857b2a
toni <tkukurin@gmail.com>
2018-03-23 15:27:30 +0100

    tweet normalization script

--- syntaxnet/data_process.py
+++ syntaxnet/data_process.py
@@ -12,18 +12,32 @@ def compose(*fs):
     functools.reduce(lambda v, f: f(v), fs, value)
 
 
-def remove_crap(tweet_content):
-  atreplies = r'(@[A-Za-z0-9_]+)'
-  urls = r'http:\S+'
+def regex_normalize(match):
+  AT, URL, HASHTAG = 1, 2, 3
+  if match.group(AT):
+    return ' @user '
+  if match.group(URL):
+    return ' @url '
+  if match.group(HASHTAG):
+    return ' @hashtag '
+  return ' '
 
-  regex = '|'.join(
-      v for k, v in locals().items()
-      if k != 'tweet_content')
-  return re.sub(regex, '', tweet_content)
+
+def remove_crap(tweet_content):
+  regex = '|'.join([
+    # @reply
+    '(@[A-Za-z0-9_]+)',
+    # URL (just guess abc.xyz is URL). don't capture inner groups.
+    '((?:http[s]?://)?(?:www\.)?\S+\.[a-z]{2,6}/?\S*)',
+    # hashtag
+    '(#\S+)',
+    # punctuation will be tagged by SyntaxNet, probably can extract from there.
+    # '(\.,-:;)',
+  ])
+  return re.sub(regex, regex_normalize, tweet_content)
 
 
 def normalize(tweets):
-  print(tweets)
   pipeline = compose(
       str.lower,
       remove_crap,
@@ -35,5 +49,7 @@ def normalize(tweets):
 if __name__ == '__main__':
   tweets = pd.read_json(TWEET_FILE)
   content = tweets['body'].apply(lambda dict_: dict_['content'])
-  print(list(normalize(content[:1])))
+  with open(os.path.join(DATA, 'tweets_normalized.txt'), 'w') as f:
+    for tweet in normalize(content):
+      print(tweet, file=f)
 
