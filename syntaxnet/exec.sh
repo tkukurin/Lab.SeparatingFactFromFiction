7d9dc7d7f63c332f2931cbc1044c78b36bbfb7e1
toni <tkukurin@gmail.com>
2018-03-24 12:47:06 +0100

    update run scripts

---
+++ syntaxnet/exec.sh
@@ -0,0 +1,2 @@
+#!/bin/bash
+docker exec syntaxnet-wrap python /usr/local/runner/wrapper.py $1
--- syntaxnet/run.sh
+++ syntaxnet/run.sh
@@ -1,8 +1,17 @@
 #!/bin/bash
 DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")/.." && pwd)"
 
-docker run --name syntaxnet --rm -ti -p 8888:8888 -d \
+# "barebones" syntaxnet
+#docker run --name syntaxnet --rm -ti -p 8888:8888 -d \
+#    -v "$DIR/data":/data \
+#    -v "$DIR/syntaxnet":/opt/tensorflow/syntaxnet/examples/local \
+#    -v /home/toni/nlp_data/conll17:/models \
+#    tensorflow/syntaxnet
+
+# parser with wrapper
+docker run -d --rm -it \
+    --name syntaxnet-wrap \
     -v "$DIR/data":/data \
-    -v "$DIR/syntaxnet":/code \
-    -v /home/toni/nlp_data/conll17:/models \
-    tensorflow/syntaxnet
+    -v "$DIR/syntaxnet":/usr/local/runner \
+    -v ~/nlp_data/conll17:/usr/local/tfmodels/ \
+    nardeas/tensorflow-syntaxnet
