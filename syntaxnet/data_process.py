090180bff1525d1be2c92b8057fc06342c76b453
toni <tkukurin@gmail.com>
2018-04-10 11:34:27 +0200

    fixed some processing

--- syntaxnet/data_process.py
+++ syntaxnet/data_process.py
@@ -3,33 +3,43 @@ import pandas as pd
 from util import compose, const
 
 
+# replace with @[a-zA-Z] since this is username format for Twitter
+regex_key_value = [
+  # mail
+  ('@email', r'([a-zA-Z0-9_.+-]+@[a-zA-Z0-9-]+\.[a-zA-Z0-9-.]+)'),
+  # @reply
+  # people usually use @user.com for e.g. @nytimes.com
+  ('@user', '(@[A-Za-z0-9\_]+(?:.com)?)'),
+  # URL; just guess abc.xyz is a URL. don't capture inner groups.
+  ('@url',
+  '('
+  '(?:(?:https?:\/\/|www\.)\S*)'
+  '|'
+  '(?:[d\w]+\.)+(?:[a-z]{2,6}(?:\.[a-z]{,6})?\S*)'
+  ')'),
+  # hashtag
+  # https://stackoverflow.com/questions/8451846/actual-twitter-format-for-hashtags-not-your-regex-not-his-code-the-actual
+  # punctuation terminates hashtag, must be preceeded by space
+  ('@hashtag', '(#[A-Za-z0-9\_]+)'),
+]
+keys = [k for k, v in regex_key_value]
+values = [v for k, v in regex_key_value] + [
+  # punctuation will be tagged by SyntaxNet,
+  # remove newline for easier input
+  '\\n',
+  '\\r',
+]
+regex_str = '|'.join('\\s*{}\\s*'.format(i) for i in values)
+
 def regex_normalize(match):
-  AT, URL, HASHTAG = 1, 2, 3
-  if match.group(AT):
-    return ' @user '
-  if match.group(URL):
-    return ' @url '
-  if match.group(HASHTAG):
-    return ' @hashtag '
+  for match_group, replacement in enumerate(keys, start=1):
+    if match.group(match_group):
+      return ' {} '.format(replacement)
   return ' '
 
 
 def remove_crap(tweet_content):
-  # also remove surrounding whitespace
-  regex = '|'.join('\\s*{}\\s*'.format(i) for i in [
-    # @reply
-    '(@[A-Za-z0-9_]+)',
-    # URL (just guess abc.xyz is URL). don't capture inner groups.
-    '((https?:\/\/)?([\da-z\.-]+)\.([a-z\.]{2,6})([\/\w\?\=\&\.-]*)*\/?\S)',
-    # hashtag
-    '(#\S+)',
-    # punctuation will be tagged by SyntaxNet,
-    # remove newline for easier input
-    '\\n',
-    '\\r',
-    # '(\.,-:;)',
-  ])
-  return re.sub(regex, regex_normalize, tweet_content)
+  return re.sub(regex_str, regex_normalize, tweet_content)
 
 
 def normalize(tweets):
--- test/test_normalize.py
+++ test/test_normalize.py
@@ -11,9 +11,16 @@ class TestNormalize(unittest.TestCase):
         normalize([x]), 'this is a newline tweet')
 
   def test_url(self):
-    x = 'a website calledwww.google.com test'
+    x = 'a website called pic.twitter.com/url test'
     self.assertEqualIterable(
         normalize([x]), 'a website called @url test')
+
+  def test_username_dotcom(self):
+    x = 'US wary of lease of port in Australia; Marine base could be target of ' \
+        'spying @nytimes.com'
+    self.assertEqualIterable(
+        normalize([x]), 'us wary of lease of port in australia; '
+                        'marine base could be target of spying @user')
 
   def test_url_http(self):
     url = 'http://cultura.elpais.com/cultura/2016/03/22/actualidad' \
@@ -30,11 +37,54 @@ class TestNormalize(unittest.TestCase):
     self.assertEqualIterable(
         normalize([x]), 'a website called @url test')
 
+  def test_url_ampersand_percent_hashtag_underscore_comma_plus_colon(self):
+    url = 'http://www.vox.com/2016/3/24/11296168/down-with-diet-books?utm_campaign' \
+          '=vox&utm_content=feature%3A#fixed&utm_medium=social&utm_source' \
+          '=twitter+test:test2,,comma'
+    x = 'a website called {} test'.format(url)
+    self.assertEqualIterable(
+        normalize([x]), 'a website called @url test')
+
   def test_url_nospace_https(self):
     url = 'https://google.co.uk/123/abc/xyz?key=value&k2=v2'
     x = 'a website called{} test'.format(url)
     self.assertEqualIterable(
         normalize([x]), 'a website called @url test')
+
+  def test_url_no_www(self):
+    url = 'google.co.uk/123/abc/xyz?key=value&k2=v2#test'
+    x = 'a website called {} test'.format(url)
+    self.assertEqualIterable(
+        normalize([x]), 'a website called @url test')
+
+  def test_link_username_hashtag(self):
+    x = 'Starbucks will start donating 100% of its unused food to those in need ' \
+        'http://mashable.com/2016/03/22/starbucks-food-donations-foodshare/' \
+        '#WQ2TeNi9q059 ... via @mashable  #welldoneStarbucks'
+    self.assertEqualIterable(
+        normalize([x]), 'starbucks will start donating 100% of its unused '
+                        'food to those in need @url ... via @user  '
+                        '@hashtag')
+
+  def test_no_url(self):
+    x = 'confessed to trying...'
+    self.assertEqualIterable(
+        normalize([x]), 'confessed to trying...')
+
+  def test_hashtag_in_sequence(self):
+    x = 'my #first #second #third these are all #hashtags'
+    self.assertEqualIterable(
+        normalize([x]), 'my @hashtag  @hashtag  @hashtag these are all '
+                        '@hashtag')
+
+  def test_hashtag_from_text(self):
+    x = '@AJEnglish @marwanbishara It only proved that #Syria is another ' \
+        '#Libya #Iraq. For long, #war is fought far on foreign soil.'
+    self.assertEqualIterable(
+        normalize([x]), '@user @user it only proved that @hashtag is another'
+                        '@hashtag @hashtag. for long, @hashtag is fought far'
+                        'on foreign soil.')
+
 
   def assertEqualIterable(self, got, expect):
     self.assertEqual(next(got), expect)
